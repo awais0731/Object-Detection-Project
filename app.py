374cf0b1b748693039a4d69579d1227527a731a1
awais.raza <awais.raza@pitb.gov.pk>
2024-08-06 15:02:35 +0500

    add notebook experiement

---
+++ app.py
@@ -0,0 +1,4 @@
+import torch
+
+if __name__ == "__main__":
+    print("PyTorch version:", torch.__version__)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.1',
+    author= 'Awais',
+    author_email= 'awaisraza0731@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
