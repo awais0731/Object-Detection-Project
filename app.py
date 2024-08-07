8ecb2180736b5a24e9b918ccc52e0bdc01e4eb53
awais.raza <awais.raza@pitb.gov.pk>
2024-08-07 16:17:21 +0500

    add model trainer component

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from signLanguage.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == "__main__":
+
     
     obj = TrainPipeline()
     obj.run_pipeline()
--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -3,11 +3,14 @@ from signLanguage.logger import logging
 from signLanguage.exception import SignException
 from signLanguage.components.data_ingestion import DataIngestion
 from signLanguage.components.data_validation import DataValidation
+from signLanguage.components.model_trainer import ModelTrainer
 
 from signLanguage.entity.config_entity import (DataIngestionConfig,
-                                               DataValidationConfig)
+                                               DataValidationConfig,
+                                               ModelTrainerConfig)
 from signLanguage.entity.artifacts_entity import (DataIngestionArtifact,
-                                                  DataValidationArtifact,)
+                                                  DataValidationArtifact,
+                                                  ModelTrainerArtifact)
 
 
 
@@ -15,6 +18,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
 
@@ -63,6 +67,18 @@ class TrainPipeline:
         except Exception as e:
             raise SignException(e, sys) from e
         
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise SignException(e, sys)
+        
 
     
     def run_pipeline(self) -> None:
@@ -72,7 +88,9 @@ class TrainPipeline:
                 data_ingestion_artifact=data_ingestion_artifact
             )
             if data_validation_artifact.validation_status == True:
-               print("validation true")
+
+                model_trainer_artifact = self.start_model_trainer()
+                
 
             else:
                 raise Exception("Your data is not in correct format")
